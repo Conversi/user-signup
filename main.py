5a0518b2b3888671189fb6bde3d1d175146f618d
Brian D. Conversi <bdconversi@yahoo.com>
2018-10-21 11:49:19 -0500

    I don't understand why this doesn't work

--- main.py
+++ main.py
@@ -3,34 +3,46 @@ from flask import Flask, request, redirect, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-def username():
-    user = request.form['username']
-    if len(user) < 3 or len(user) > 20 or " " in user:
-        return render_template('index.html', username=username)
+@app.route("/index", methods=['POST'])
+def validation():
 
-def password():
-    pasw = request.form['password']
-    if len(pasw) < 3 or len(pasw) > 20 or " " in pasw:
-        return "That is not a valid password"
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+        email = request.form['email']
 
-def verify():
-    pasw = request.form['password']
-    veri = request.form['verify_password']
-    if veri != pasw:
-        return "Passwords don't match"
+        user_error = ''
+        password_error = ''
+        verify_error = ''
+        email_error = ''
 
-def email():
-    emai = request.form['email']
-    if len(emai) < 3 or len(emai) > 20 or  " " in emai or emai.count("@") != 1 or emai.count(".") != 1:
-        return "That's not a valid email"
+        if len(username) < 3 or len(username) > 20 or " " in username:
+                user_error = 'Invalid User Name'
+
+        elif len(password) < 3 or len(password) > 20 or " " in password:
+                password_error = 'That is not a valid password'
+                password = ''
+
+        elif verify != password:
+                verify_error = 'Passwords do not match'
+                verify = ''
+
+        elif len(email) < 3 or len(email) > 20 or  " " in email or email.count("@") != 1 or email.count(".") != 1:
+                email_error = 'That is not a vailid email'
+        
+        elif not user_error and not password_error and not verify_error and not email_error:
+                return redirect('/welcome')
+        else:
+                return render_template('index.html', username=username, password=password, verify=verify, email=email,user_error=user_error,
+                password_error=password_error, verify_error=verify_error, email_error=email_error)
 
 @app.route("/welcome", methods=['POST'])
 def welcome():
-    user_name = request.form['username']
-    return render_template('welcome.html', username = user_name)
+    username = request.form['username']
+    return render_template('welcome.html', username=username)
 
-@app.route("/", methods=['POST', 'GET'])
-def index():
+@app.route("/")
+def display():
     return render_template('index.html')
 
 app.run()
